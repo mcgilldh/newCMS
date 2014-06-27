4062203d850fa0e85b92e3bfd3008e3570343675
Jonathan Eidelman <jonathaneidelman@Jonathans-MacBook-Air.local>
2014-06-27 14:55:30 -0400

    Team section is now function

---
+++ team.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+  </head>
+
+  <body>
+    <?php
+       include("includes/phpheader.php");
+       include ($thisabspath."/includes/cssheader.php");
+       include ($thisabspath."/includes/javaheader.php");
+    ?>
+    <script src="team_fns.js"> </script>
+    <div id="teamContent">
+      <h1>Our Team</h1>
+      <div class="box">
+	<h4><a href="#" onclick="displayBio('mcgill', 'McGill University');">McGill University</a></h4>
+	<ul class="list">
+	  <li><a href="#" onclick="displayBio('cbradley', 'Catherine Bradley');">Catherine Bradley</a>: Co-Founder of the Virtual Textile Project<br>McGill Project Co-Director</li>
+	  <li><a href="#" onclick="displayBio('ssinclair', 'Stefan Sinclair');">Stefan Sinclair</a><br>Collaborator</li>
+	  <li><a href="#" onclick="displayBio('mmilner', 'Matthew Milner');">Matthew Milner</a><br>Web Design</li>
+	  <li><a href="#" onclick="displayBio('pdavoust', 'Peter Davoust');">Peter Davoust</a><br>Developer</li>
+	  <li><a href="#" onclick="displayBio('jeidelman', 'Jonathan Eidelman');">Jonathan Eidelman</a><br>Developer</li>
+	</ul>
+	<h4><a href="#" onclick="displayBio('mcgill', 'McGill University');">Dragon and Phoenix Software Inc.</a></h4>
+	<ul class="list">
+	  <li><a href="#" onclick="displayBio('klind', 'Kat Lind');">Kat Lind</a>: Co-Founder of the Virtual Textile Project<br>
+	    Dragon and Phoenix Project Co-Director</li>
+	</ul>
+      </div>
+    </div>
+
+
+    <?php
+       include($thisabspath."/includes/footer.php");
+    ?>
+  </body>
+</html>
---
+++ teamServer.php
@@ -0,0 +1,19 @@
+<?php
+include("includes/dbconfig.php");
+include("includes/connection.php");
+include("includes/functions.php");
+$get = $_GET;
+
+if ($get['mode']=="getBio") {
+    $userid = $get['id'];
+    $fullname = $get['name'];
+    $thisuser=grabinfo('cms_users','userid',$userid,1);
+
+    $bio = nl2br(stripslashes($thisuser['bio']));
+
+    $response = array("name" => $fullname, "bio" => $bio);
+    echo json_encode($response);
+    return;
+}
+
+?>
